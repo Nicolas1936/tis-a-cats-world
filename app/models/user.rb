a1f0160f9aadb79f43e999b582f44ce0eed28458
Nicolas Blanchard <blanchard.nicolas@hotmail.com>
2022-12-03 02:00:46 +0100

    new chatroom

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :chatrooms_as_asker, class_name: "Chatroom", foreign_key: :asker_id
+  has_many :chatrooms_as_receiver, class_name: "Chatroom", foreign_key: :receiver_id
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
