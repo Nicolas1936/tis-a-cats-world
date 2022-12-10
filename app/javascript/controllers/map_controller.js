f9f4c4281dcccd6a463b63e9c1e666749d5e9db8
Nicolas Blanchard <blanchard.nicolas@hotmail.com>
2022-12-10 09:10:47 +0100

    new

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -2,6 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
 
 // Connects to data-controller="map"
+
 export default class extends Controller {
   static values = {
     apiKey: String,
