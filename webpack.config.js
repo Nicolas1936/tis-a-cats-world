b41c8af534201a8b22b558d9369cd9139e307ebe
Nicolas Blanchard <blanchard.nicolas@hotmail.com>
2022-12-11 15:34:42 +0100

    test

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require("webpack")
 
 module.exports = {
   mode: "production",
-  devtool: "source-map",
+  devtool: false,
   entry: {
     application: "./app/javascript/application.js"
   },
@@ -12,10 +12,10 @@ module.exports = {
     sourceMapFilename: "[file].map",
     path: path.resolve(__dirname, "app/assets/builds"),
   },
-  // performance: {
-  //   maxEntrypointSize: 512000,
-  //   maxAssetSize: 512000
-  // },
+  performance: {
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  },
   plugins: [
     new webpack.optimize.LimitChunkCountPlugin({
       maxChunks: 1
