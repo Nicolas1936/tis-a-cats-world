6ecbca784e84b09e0a8f1fbfd1b3c6d36806c45a
Nicolas Blanchard <blanchard.nicolas@hotmail.com>
2022-12-15 18:17:26 +0100

    fix bug cat show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,22 +29,29 @@ class UsersController < ApplicationController
   end
 
   def connect_chatrooms
-    receiver = User.find(params[:id])
-    asker = current_user
 
-    # if a chatrooms already existe with this reveiver and asker
-    chatroom = Chatroom.where(asker:asker, receiver:receiver)
+    if user_signed_in?
 
-    if chatroom.any?
-      # Go inside this chatroom
-      redirect_to messages_path(chatroom_id:chatroom.first.id)
-    # else
+      receiver = User.find(params[:id])
+      asker = current_user
+
+      # if a chatrooms already existe with this reveiver and asker
+      chatroom = Chatroom.where(asker:asker, receiver:receiver)
+
+      if chatroom.any?
+        # Go inside this chatroom
+        redirect_to messages_path(chatroom_id:chatroom.first.id)
+      # else
+      else
+        # Create a new chatroom with this 2 account
+        new_chatroom = Chatroom.create!(asker:asker, receiver:receiver)
+        # Go inside this new chatroom
+        redirect_to messages_path(chatroom_id:new_chatroom.id)
+      # end
+      end
+
     else
-      # Create a new chatroom with this 2 account
-      new_chatroom = Chatroom.create!(asker:asker, receiver:receiver)
-      # Go inside this new chatroom
-      redirect_to messages_path(chatroom_id:new_chatroom.id)
-    # end
+      redirect_to new_user_session_path
     end
 
   end
